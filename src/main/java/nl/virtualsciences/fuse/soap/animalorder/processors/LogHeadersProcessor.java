d2fe890ec45b63b9247323851d2aa24a991dbbb5
Martien van den Akker <martien.van.den.akker@virtualsciences.nl>
2021-03-09 10:50:46 +0100

    Synch with example GitLab.

--- src/main/java/nl/virtualsciences/fuse/soap/animalorder/processors/LogHeadersProcessor.java
+++ src/main/java/nl/virtualsciences/fuse/soap/animalorder/processors/LogHeadersProcessor.java
@@ -8,14 +8,26 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 public class LogHeadersProcessor implements Processor {
 	private final Logger log = LoggerFactory.getLogger(LogHeadersProcessor.class);
 
-	@Override
+	@Value("${aOSvc.endpoint}")
+	private String animalOrderSvcEndpoint;
+
+	@Value("${aOSvc.endpoint.message}")
+	private String animalOrderSvcMessage;
+
 	public void process(Exchange exchange) throws Exception {
 
 		Object bodyObject = exchange.getIn().getBody();
+
+		String svcEndpointPrefix = System.getenv().getOrDefault("SERVICE_ENDPOINT_PREFIX", "AapNootMiesService: ");
+		
+
+		log.info( String.format(animalOrderSvcMessage, svcEndpointPrefix, animalOrderSvcEndpoint));
+		log.info("Endpoint of AnimalOrderService: " + animalOrderSvcEndpoint);
 		log.info("CanonicalClassName of Body: " + bodyObject.getClass().getCanonicalName());
 
 		if (ArrayList.class.isAssignableFrom(bodyObject.getClass())) {
